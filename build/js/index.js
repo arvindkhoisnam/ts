70ed54fbe753e9c6893f4059bf60ee13fc64a396
arvindkhoisnam <arvindkhoisnam@Arvinds-MacBook-Pro-2.local>
2024-01-14 15:11:20 +0530

    practise session 2

--- build/js/index.js
+++ build/js/index.js
@@ -1,9 +1,4 @@
 "use strict";
-function add(a, b) {
-    return a + b;
-}
-const result = add(2, 9);
-console.log(result);
 const tool = {
     name: "Tool",
     active: true,
@@ -11,4 +6,52 @@ const tool = {
     genre: "progressive rock",
     grammy: 4,
 };
-console.log(tool);
+// console.log(tool.albums);
+/////////////////////////////////////////////////////////////////////////
+// Literal types
+let myName;
+let userName;
+/////////////////////////////////////////////////////////////////////////
+//functions
+// functions that do not return anything implicitly returns a void
+function logMsg(message) {
+    console.log(message);
+}
+function add(a, b) {
+    return a + b;
+}
+// interface mathFunc {
+//   (a: number, b: number): number;
+// }
+let multuply = (a, b) => a * b;
+// optional params
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined")
+        return a + b + c;
+    return a + b;
+};
+console.log(addAll(2, 2));
+// default params
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+console.log(sumAll(2, 2));
+// Rest params
+const total = (...nums) => {
+    return nums.reduce((a, b) => a + b, 0);
+};
+logMsg(total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+// never type
+// function createError(err: string): never {
+//   throw new Error(err);
+// }
+const createError = (err) => {
+    throw new Error(err);
+};
+const numOrString = (value) => {
+    if (typeof value === "string")
+        return "string";
+    if (typeof value === "number")
+        return "number";
+    return createError("this should never happen");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
+//type aliases
+type numOrBool = number | boolean;
+type stringOrNum = string | number;
 
-const result = add(2, 9);
-console.log(result);
+type userId = stringOrNum;
 
-type band = {
+type Band = {
   name: string;
   active: boolean;
   albums: string[];
   genre: string;
-  grammy: number | boolean;
+  grammy: numOrBool;
 };
 
-const tool: band = {
+const tool: Band = {
   name: "Tool",
   active: true,
   albums: ["Laterealus", "10,000 days", "Fear Innoculum"],
@@ -21,4 +20,67 @@ const tool: band = {
   grammy: 4,
 };
 
-console.log(tool);
+// console.log(tool.albums);
+/////////////////////////////////////////////////////////////////////////
+
+// Literal types
+
+let myName: "John";
+let userName: "John" | "Amy" | "Karl";
+
+/////////////////////////////////////////////////////////////////////////
+
+//functions
+
+// functions that do not return anything implicitly returns a void
+function logMsg(message: any) {
+  console.log(message);
+}
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+type mathFunc = (a: number, b: number) => number;
+
+// interface mathFunc {
+//   (a: number, b: number): number;
+// }
+
+let multuply: mathFunc = (a, b) => a * b;
+
+// optional params
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") return a + b + c;
+  return a + b;
+};
+console.log(addAll(2, 2));
+
+// default params
+const sumAll = (a: number, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+console.log(sumAll(2, 2));
+
+// Rest params
+const total = (...nums: number[]): number => {
+  return nums.reduce((a, b) => a + b, 0);
+};
+
+logMsg(total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// never type
+
+const createError = (err: string) => {
+  throw new Error(err);
+};
+
+// function createError(err: string): never {
+//   throw new Error(err);
+// }
+
+const numOrString = (value: number | string): string => {
+  if (typeof value === "string") return "string";
+  if (typeof value === "number") return "number";
+  return createError("this should never happen");
+};
